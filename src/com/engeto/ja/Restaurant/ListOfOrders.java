a573aa483ed72d89ee5166e5fecd66d3a72f6b8f
Zuzana Tománková <Tomankova.Z@seznam.cz>
2024-03-02 13:53:57 +0100

    Vytvoření třídy ListOfOrders

---
+++ src/com/engeto/ja/Restaurant/ListOfOrders.java
@@ -0,0 +1,13 @@
+package com.engeto.ja.Restaurant;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListOfOrders {
+private List<Orders> ordersList = new ArrayList<>();
+    public void addOrder(Orders orders) {ordersList.add(orders);
+    }
+    public void removeOrder(int index) {ordersList.remove(index);
+    }
+
+}
